0c6fe400cd9f55601371992dc9dfa8c89a577d88
Dmitry Harnitski <dmitry@brainjocks.com>
2015-01-28 21:47:28 -0500

    ruby day 3 before tests

--- ruby/day2study3.rb
+++ ruby/day2study3.rb
@@ -4,9 +4,6 @@ line_num=0
 File.open('day2study3.rb').each do |line|
 
   line_num += 1
-
-  if ( line =~ /line/ )
-    puts "#{line_num} #{line}"
-  end
+  puts "#{line_num} #{line}" if ( line =~ /line/ )
 
 end
